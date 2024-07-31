031b623f3a24c803da00a4fbbc8d413c7568b272
Viraj Patel <viraj.patel@example.com>
2024-07-31 11:59:44 +0530

    added searchview in homefragment

--- app/src/main/java/com/my/audio_video_fm/fragment/HomeFragment.java
+++ app/src/main/java/com/my/audio_video_fm/fragment/HomeFragment.java
@@ -74,6 +74,7 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 NavController navController = NavHostFragment.findNavController(HomeFragment.this);
                 navController.navigate(R.id.searchFragment);
+
             }
         });
         return view;
