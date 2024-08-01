04b61fec2cfe2b7b4c6886d5374b194281b06ab0
Viraj Patel <viraj.patel@example.com>
2024-08-01 12:07:39 +0530

    studio 3 tablayout record,my audio,anylytics

--- app/src/main/java/com/my/audio_video_fm/adapter/ViewPagerAdapterStudio.java
+++ app/src/main/java/com/my/audio_video_fm/adapter/ViewPagerAdapterStudio.java
@@ -7,10 +7,9 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.my.audio_video_fm.fragment.AnalyticsFragment;
 import com.my.audio_video_fm.fragment.MyAudioFragment;
-import com.my.audio_video_fm.fragment.MyListFragment;
 import com.my.audio_video_fm.fragment.RecordFragment;
 
-public class ViewPagerAdapterStudio  extends FragmentStateAdapter {
+public class ViewPagerAdapterStudio extends FragmentStateAdapter {
 
     public ViewPagerAdapterStudio(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
@@ -20,12 +19,14 @@ public class ViewPagerAdapterStudio  extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
         switch (position) {
-            case 1:
+            case 0:
                 return new RecordFragment();
-            case 2:
+            case 1:
                 return new MyAudioFragment();
-            default:
+            case 2:
                 return new AnalyticsFragment();
+            default:
+                return new RecordFragment();
         }
     }
 
--- app/src/main/java/com/my/audio_video_fm/fragment/MyAudioFragment.java
+++ app/src/main/java/com/my/audio_video_fm/fragment/MyAudioFragment.java
@@ -1,66 +1,66 @@
 package com.my.audio_video_fm.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.my.audio_video_fm.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyAudioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyAudioFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyAudioFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyAudioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyAudioFragment newInstance(String param1, String param2) {
-        MyAudioFragment fragment = new MyAudioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_audio, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_audio, container, false);
+        TextView filterTextView = view.findViewById(R.id.filter_text_view);
+        filterTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet();
+            }
+        });
+        TextView sortByTextView = view.findViewById(R.id.sortby);
+        sortByTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheet1();
+            }
+        });
+        return view;
+    }
+
+    private void showBottomSheet() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(requireActivity());
+        View sheetView = LayoutInflater.from(requireActivity()).inflate(R.layout.bottom_sheet_layout, null);
+        bottomSheetDialog.setContentView(sheetView);
+        bottomSheetDialog.show();
+    }
+    private void showBottomSheet1() {
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(requireActivity());
+        View sheetView = LayoutInflater.from(requireActivity()).inflate(R.layout.bottom_sheet_sort_layout, null);
+        bottomSheetDialog.setContentView(sheetView);
+
+        // Optionally, handle radio button selections here
+        RadioGroup radioGroup = sheetView.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = sheetView.findViewById(checkedId);
+                // Handle selection
+                // For example, you can show a Toast or update some data
+                Toast.makeText(requireActivity(), "Selected: " + selectedRadioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomSheetDialog.show();
     }
 }
